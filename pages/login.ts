d875e242400131f34d0b887905a4f688c1b36e71
tooniez <tooni22@proton.me>
2023-07-16 11:36:08 +0930

    test: :white_check_mark: add logon commands

--- pages/login.ts
+++ pages/login.ts
@@ -17,5 +17,25 @@ export class LoginPage {
     this.Password = page.locator('[data-test="password"]');
     this.LogonButton = page.locator('[data-test="login-button"]');
   }
+
+  // Navigate to the root URL ("/")
+  async goto() {
+    await this.page.goto("/");
+  }
+
+  // Verify that the page is present
+  async verifyPagePresent() {
+    // Check if the LogonButton is visible
+    await expect(this.LogonButton).toBeVisible();
+  }
+
+  // Login with the provided username and password
+  async LoginWithCredentials(username: string, password: string) {
+    // Fill in the Username and Password fields
+    await this.Username.fill(username);
+    await this.Password.fill(password);
+
+    // Click on the first LogonButton element
+    await this.LogonButton.first().click();
   }
 }
