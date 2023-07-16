c6dbd33f5d5bb333aaf116ec4a3b04341c2a8ee7
tooniez <tooni22@proton.me>
2023-07-16 20:40:08 +0930

    test: :sparkles: add initial products tests

--- tests/e2e/products.spec.ts
+++ tests/e2e/products.spec.ts
@@ -1,28 +1,47 @@
-import { test, expect } from '@playwright/test';
+// Products/Cart test case
+import { test } from '@playwright/test';
+import { LoginPage } from '@pages/login';
+import { Products } from '@pages/products';
 
-test('User Flow 1 - Log in as a standard user and complete checkout and order successfully', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="password"]').click();
-  await page.locator('[data-test="password"]').fill('secret_sauce');
-  await page.locator('[data-test="login-button"]').click();
-  await page.getByText('Products').click();
-  await page.locator('.inventory_list').click();
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
-  await page.locator('a').filter({ hasText: '1' }).click();
-  await page.getByText('Your Cart').click();
-  await page.locator('[data-test="checkout"]').click();
-  await page.locator('[data-test="firstName"]').click();
-  await page.locator('[data-test="firstName"]').fill('Tony');
-  await page.locator('[data-test="firstName"]').press('Tab');
-  await page.locator('[data-test="lastName"]').fill('Luu');
-  await page.locator('[data-test="lastName"]').press('Tab');
-  await page.locator('[data-test="postalCode"]').fill('5095');
-  await page.locator('[data-test="continue"]').click();
-  await page.locator('#checkout_summary_container').click();
-  await page.locator('[data-test="finish"]').click();
-  await page.getByRole('heading', { name: 'Thank you for your order!' }).click();
-  await page.getByText('Your order has been dispatched, and will arrive just as fast as the pony can get').click();
-  await page.locator('[data-test="back-to-products"]').click();
+test.beforeEach(async ({ page }) => {
+  // Create a new instance of the LoginPage
+
+  const loginPage = new LoginPage(page);
+
+  // Navigate to the login page
+  await loginPage.goto();
+  await loginPage.verifyPagePresent();
+  await loginPage.LoginWithCredentials('standard_user', 'secret_sauce');
 });
+
+test.describe('Product/Cart', () => {
+  test('Cart should be empty be default', async ({ page }) => {
+    // Create a new instance of the ProductsPage
+    const productsPage = new Products(page);
+
+    // Verify that the cart is empty be default
+    await productsPage.verifyCartEmpty();
+  });
+
+  test('should contain Add to Cart', async ({ page }) => {
+    // Create a new instance of the ProductsPage
+    const productsPage = new Products(page);
+
+    // Add an item to the cart
+    await productsPage.addItemtoCart();
+
+    // Verify that the item was added
+    await productsPage.verifyItemAdded();
+  });
+
+  test('Cart should contain count when product is added', async ({ page }) => {
+    // Create a new instance of the ProductsPage
+    const productsPage = new Products(page);
+
+    // Add an item to the cart
+    await productsPage.addItemtoCart();
+    // Verify that count notification
+    await productsPage.verifyCartCount();
+  });
+});
+
