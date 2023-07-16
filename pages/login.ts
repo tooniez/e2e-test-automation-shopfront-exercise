3b6f4a41e3b70a58616f91cf0a1579458e24496a
tooniez <tooni22@proton.me>
2023-07-16 11:33:56 +0930

    feat: :building_construction: define logon page types

--- pages/login.ts
+++ pages/login.ts
@@ -3,6 +3,12 @@ import { expect, type Locator, type Page } from "@playwright/test";
 
 export class LoginPage {
   readonly page: Page;
+
+  // Locators for the elements on the page
+  readonly Username: Locator;
+  readonly Password: Locator;
+  readonly LogonButton: Locator;
+
   constructor(page: Page) {
     this.page = page;
   }
