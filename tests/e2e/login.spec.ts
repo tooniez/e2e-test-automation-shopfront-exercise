475b36089c90582982f6566b419a83d43ce994a8
tooniez <tooni22@proton.me>
2023-07-16 11:46:01 +0930

    style: :lipstick: reformat logon test

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -1,11 +1,11 @@
 // Logon test case
-import { test } from '@playwright/test';
-import { LoginPage } from '@pages/login';
+import {test} from '@playwright/test';
+import {LoginPage} from '@pages/login';
 
-test('Logon - Log on as Standard User', async ({ page }) => {
+test('Logon - Log on as Standard User', async ({page}) => {
   // Create a new instance of the LoginPage
   const loginPage = new LoginPage(page);
-  
+
   // Navigate to the login page
   await loginPage.goto();
   await loginPage.verifyPagePresent();
@@ -14,5 +14,4 @@ test('Logon - Log on as Standard User', async ({ page }) => {
   // Initial verify that the user is logged in
   await page.getByText('Swag Labs').click();
   await page.locator('.inventory_list').click();
-  
 });
