4f47555afbc4ab1b31a5165be622b94b0c3528fe
tooniez <tooni22@proton.me>
2023-07-16 11:41:33 +0930

    test: :white_check_mark: add initial logon test steps

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -6,4 +6,13 @@ test('Logon - Log on as Standard User', async ({ page }) => {
   // Create a new instance of the LoginPage
   const loginPage = new LoginPage(page);
   
+  // Navigate to the login page
+  await loginPage.goto();
+  await loginPage.verifyPagePresent();
+  await loginPage.LoginWithCredentials('standard_user', 'secret_sauce');
+
+  // Initial verify that the user is logged in
+  await page.getByText('Swag Labs').click();
+  await page.locator('.inventory_list').click();
+  
 });
